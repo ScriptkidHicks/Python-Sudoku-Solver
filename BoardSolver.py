3e3392025aa0460252141d2f89885afcaf62e80b
Tammashicks <tammas@uoregon.edu>
2021-09-09 14:30:44 -0700

    Added a naked solve which checks for change in the board each iteration

--- BoardSolver.py
+++ BoardSolver.py
@@ -38,3 +38,67 @@ class Solver:
                     if safety[0] == "error":
                         return "error"
         return "safe", changed
+
+    def solve_sq(self, sq_index):
+        changed = False
+        taken = [element.value for element in self.board.squares[sq_index] if element.value is not None]
+        for element in self.board.squares[sq_index]:
+            if element.solved:
+                pass
+            else:
+                for value in taken:
+                    safety = element.eliminate_possible(value)
+                    if safety[1] is True:
+                        changed = True
+                    if safety[0] == "error":
+                        return "error"
+        return "safe", changed
+
+    def solve_by_rows(self):
+        changed = False
+        for x in range(9):
+            safety = self.solve_row(x)
+            if safety[1] is True:
+                changed = True
+            if safety[0] == "error":
+                return "error", changed
+        return "safe", changed
+
+    def solve_by_cols(self):
+        changed = False
+        for x in range(9):
+            safety = self.solve_col(x)
+            if safety[1] is True:
+                changed = True
+            if safety[0] == "error":
+                return "error", changed
+        return "safe", changed
+
+    def solve_by_squares(self):
+        changed = False
+        for x in range(9):
+            safety = self.solve_sq(x)
+            if safety[1] is True:
+                changed = True
+            if safety[0] == "error":
+                return "error", changed
+        return "safe", changed
+
+    def naked_solve(self):
+        changed = False
+        safety = self.solve_by_rows()
+        if safety[0] == "error":
+            return "error", changed
+        if safety[1] is True:
+            changed = True
+        safety = self.solve_by_cols()
+        if safety[0] == "error":
+            return "error", changed
+        if safety[1] is True:
+            changed = True
+        safety = self.solve_by_squares()
+        if safety[0] == "error":
+            return "error", changed
+        if safety[1] is True:
+            changed = True
+        return "safe", changed
